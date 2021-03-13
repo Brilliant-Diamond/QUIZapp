495ab07bb642e142e2c565a50cb8f40a14ebb9b7
kakeru-2112 <kake320_tako8@yahoo.co.jp>
2021-03-13 21:27:08 +0900

    editIntroDisplayのミスを修正

--- src/views/About.vue
+++ src/views/About.vue
@@ -115,7 +115,7 @@ export default {
       }
     },
     editIntroDisplay() {
-      if (this.edit_name_open) {
+      if (this.edit_intro_open) {
         this.edit_intro_open = false
       } else {
         this.edit_intro_open = true
